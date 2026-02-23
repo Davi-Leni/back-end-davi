4d8e1e03f3a414ec744f1df04c43ec55e33fd86f
Davi <davi.tiburcio.rosa@escola.pr.gov.br>
2026-02-23 12:31:15 -0300

    hhh

---
+++ aula.js
@@ -0,0 +1,7 @@
+//Comentário
+// Criar Variáveis
+let idade = 23; //Tipo Number
+console.log("Idade:", idade);
+let nome ="Dionatan"; // Tipo String
+console.log("Nome:", nome);
+console.log("Meu nome é", nome, "e eu tenho", idade, "anos");
